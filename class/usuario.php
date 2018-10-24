95fb69bc4e54e65200d6020fe0d4ac8947f04add
Marcelo Argenta <marcelo_argenta@hotmail.com>
2018-10-24 10:38:48 -0300

    Metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -77,12 +77,9 @@ public function loadById($id){
 
 	if(count($result) > 0){
 
-		$row = $result[0];
+		$this->setData($result[0]);
 
-		$this->setIdusuario($row['idusuario']);
-		$this->setDeslogin($row['deslogin']);
-		$this->setDessenha($row['dessenha']);
-		$this->setDtcadastro(new DateTime ($row['dtcadastro']));
+	
 	
 	}
 
@@ -116,18 +113,58 @@ public function login($login, $password){
 
 	if(count($result) > 0){
 
-		$row = $result[0];
+		
 
-		$this->setIdusuario($row['idusuario']);
-		$this->setDeslogin($row['deslogin']);
-		$this->setDessenha($row['dessenha']);
-		$this->setDtcadastro(new DateTime ($row['dtcadastro']));
-	
+		$this->setData($result[0]);
+		
 	}	
 	else{
 		throw new Exception("Login ou senha inválido!");
 	}
 
+}
+
+public function setData($data){
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime ($data['dtcadastro']
+	));
+	
+}
+
+public function insert(){
+	$sql = new Sql();
+
+	$result = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+		':LOGIN'=>$this->getDeslogin(),
+		':PASSWORD'=>$this->getDessenha()
+
+	));
+	if(count($result)> 0){
+		$this->setData($result[0]);
+	}
+}
+
+public function update($login, $password){
+
+	$this->setDeslogin($login);
+	$this->setDessenha($password);
+
+	$sql = new Sql();
+
+	$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+
+		":LOGIN"=>$this->getDeslogin(),
+		":PASSWORD"=>$this->getDessenha(),
+		":ID"=>$this->getIdusuario()
+
+	));
+}
+
+public function __construct($login = "", $password = ""){
+	$this->setDeslogin($login);
+	$this->setDessenha($password);
 }
 
 public function __toString(){
--- index.php
+++ index.php
@@ -35,9 +35,28 @@ echo json_encode($search);
 
 
 //carrega um usuario com login e senha.
-$usuario = new Usuario();
-$usuario->login("root", "!55");
+//$usuario = new Usuario();
+//$usuario->login("root", "!55");
+//echo $usuario;
 
+
+
+/*
+//INSERE UM USUARIO POR stored procedure
+$aluno = new Usuario("aluno","@lun0");
+
+
+$aluno->insert();
+
+echo $aluno;
+*/
+
+
+$usuario = new Usuario();
+
+$usuario->loadById(9);
+
+$usuario->update("professor", "1235");
 
 echo $usuario;
 
